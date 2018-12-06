96a1958d6a979c0cea355c04ec538a122bd121f1
Gabriel Santos <gabrielinflex@gmail.com>
2018-12-06 13:15:25 -0200

    remove test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
-FROM node:9-slim
+FROM nginx:alpine
+
 ENV PORT 8080
 EXPOSE 8080
-WORKDIR /usr/src/app
-COPY . .
-CMD ["npm", "start"]
+
+CMD npm run build --env=${ENV}
+
+COPY nginx.conf /etc/nginx/nginx.conf
+
+COPY dist/* /usr/share/nginx/html
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ninha-belt-ng!');
+    expect(page.getParagraphText()).toEqual('Welcome to ninja-belt-ng!');
   });
 });
